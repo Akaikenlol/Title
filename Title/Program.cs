f1e106a08fb05271255b7b3d9dbd3b391ddcd829
Akaikenlol <67327997+Akaikenlol@users.noreply.github.com>
2020-07-05 15:28:35 +0630

    Add files via upload

---
+++ Title/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Title
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("The current console title is:\"{0}\"", Console.Title);
+            Console.ReadKey(true);
+            Console.Title = "The title has changed!";
+            Console.WriteLine("Note that the new console title is \"{0}\"\n"+"(press any key to quit.",Console.Title);
+            Console.ReadKey(true);
+        }
+    }
+}
